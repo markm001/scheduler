0251399d89ee2d63f8a9987150e3f0b64d7e5aa1
markm001 <markchrim@gmail.com>
2023-01-17 14:45:32 +0100

    Added styling

--- scheduler-frontend/src/app/in-memory-data.service.ts
+++ scheduler-frontend/src/app/in-memory-data.service.ts
@@ -10,12 +10,20 @@ export class InMemoryDataService implements InMemoryDbService{
     const teams = [
         { id: 'a1', teamname:'Team-1' },
         { id: 'b2', teamname:'Team-2' },
-        { id: 'b3', teamname:'Team-3' }
+        { id: 'b3', teamname:'Team-3' },
+        { id: 'b4', teamname:'Team-4' }
     ]
 
     const members = [
       { id: 'a1', nickname: 'Member-1' },
-      { id: 'a2', nickname: 'Member-2' }
+      { id: 'a2', nickname: 'Member-2' },
+      { id: 'a3', nickname: 'Member-3' },
+      { id: 'a4', nickname: 'Member-4' },
+      { id: 'a5', nickname: 'Member-5' },
+      { id: 'a6', nickname: 'Member-6' },
+      { id: 'a7', nickname: 'Member-7' },
+      { id: 'a8', nickname: 'Member-8' },
+      { id: 'a9', nickname: 'Member-9' }
     ]
 
     //return the data as object with: { }
