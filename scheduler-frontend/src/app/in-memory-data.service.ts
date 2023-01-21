8f921fc98170d090b9fc5b5d3a741d4b09e6d7ca
markm001 <markchrim@gmail.com>
2023-01-21 20:05:27 +0100

    fixed in-memory-data.service for frontend

--- scheduler-frontend/src/app/in-memory-data.service.ts
+++ scheduler-frontend/src/app/in-memory-data.service.ts
@@ -1,32 +1,70 @@
 import {Injectable} from '@angular/core';
-import {InMemoryDbService} from "angular-in-memory-web-api";
+import {getStatusText, InMemoryDbService, RequestInfo, ResponseOptions, STATUS} from "angular-in-memory-web-api";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class InMemoryDataService implements InMemoryDbService{
+  private members = [
+    { id: 'a1', nickname: 'Member-1' },
+    { id: 'a2', nickname: 'Member-2' },
+    { id: 'a3', nickname: 'Member-3' },
+    { id: 'a4', nickname: 'Member-4' },
+    { id: 'a5', nickname: 'Member-5' },
+    { id: 'a6', nickname: 'Member-6' },
+    { id: 'a7', nickname: 'Member-7' },
+    { id: 'a8', nickname: 'Member-8' },
+    { id: 'a9', nickname: 'Member-9' }
+  ]
+  private teams = [
+    { id: 'a1', teamname:'Team-1', members: this.members },
+    { id: 'b2', teamname:'Team-2', members: {} },
+    { id: 'b3', teamname:'Team-3', members: {} },
+    { id: 'b4', teamname:'Team-4', members: {} }
+  ]
+
   constructor() { }
   createDb() {
-    const teams = [
-        { id: 'a1', teamname:'Team-1' },
-        { id: 'b2', teamname:'Team-2' },
-        { id: 'b3', teamname:'Team-3' },
-        { id: 'b4', teamname:'Team-4' }
-    ]
+    return {
+      members : this.teams,
+      teams : this.members
+    }
+  }
 
-    const members = [
-      { id: 'a1', nickname: 'Member-1' },
-      { id: 'a2', nickname: 'Member-2' },
-      { id: 'a3', nickname: 'Member-3' },
-      { id: 'a4', nickname: 'Member-4' },
-      { id: 'a5', nickname: 'Member-5' },
-      { id: 'a6', nickname: 'Member-6' },
-      { id: 'a7', nickname: 'Member-7' },
-      { id: 'a8', nickname: 'Member-8' },
-      { id: 'a9', nickname: 'Member-9' }
-    ]
+  //override the get-Method, return custom Response
+  get(requestInfo: RequestInfo): Observable<any> {
+    const responseBody = this.getResponseBody(requestInfo)
 
-    //return the data as object with: { }
-    return {teams, members}
+    return requestInfo.utils.createResponse$( () => {
+      let response:ResponseOptions = {
+        status: STATUS.OK,
+        headers: requestInfo.headers,
+        url: requestInfo.url,
+        body: responseBody
+      }
+
+      if (response.status != null) {
+        response.statusText = getStatusText(response.status)
+      }
+
+      return response
+    })
+  }
+
+  private getResponseBody(requestInfo: RequestInfo) {
+    let regexTeams = new RegExp('api\\/users\\/\\w+\\b\\/teams')
+    let regexMember = new RegExp('api\\/teams\\/\\w+\\b\\/members')
+
+    if(regexTeams.test(requestInfo.url)) {
+      return this.teams
+    } else if(regexMember.test(requestInfo.url)) {
+      //Use builtIn utils method to filter the returned list for the passed in URL-Id
+      let team = requestInfo.utils.findById(this.teams, requestInfo.id)
+      return team?.members
+    } else {
+      return { }
+    }
+
   }
 }
