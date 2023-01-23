703b662af6c146e7110eeb99eb55ac170cb2f0fb
markm001 <markchrim@gmail.com>
2023-01-23 01:35:44 +0100

    added google login with angularx-social-login

--- scheduler-frontend/src/app/app.component.ts
+++ scheduler-frontend/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GoogleLoginProvider, SocialAuthService, SocialUser} from "@abacritt/angularx-social-login";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  protected user: SocialUser | undefined
+
+  constructor(private authService:SocialAuthService) { }
   title = 'Scheduler Application';
+
+  public ngOnInit() {
+    this.authService.authState.subscribe(
+      (userData) => {
+        console.log(userData)
+
+        this.user = userData
+      })
+  }
+  public socialSignOut() {
+    this.authService.signOut().then(_ => {
+        console.log("Signed Out")
+      })
+  }
 }
--- scheduler-frontend/src/app/app.module.ts
+++ scheduler-frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import {InMemoryWebApiModule} from "angular-in-memory-web-api";
 import {InMemoryDataService} from "./in-memory-data.service";
 import {MemberComponent} from './member/member.component';
 import {environment} from "../environments/environment";
+import {GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule} from '@abacritt/angularx-social-login';
 
 @NgModule({
   declarations: [
@@ -21,10 +22,24 @@ import {environment} from "../environments/environment";
     AppRoutingModule,
     HttpClientModule,
     environment.production ? [] : InMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
+      InMemoryDataService, {dataEncapsulation: false}
+    ),
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [{
+        id: GoogleLoginProvider.PROVIDER_ID,
+        provider: new GoogleLoginProvider('330715887976-c5b9r5v68o73k1uqohddc4sgev8mu02j.apps.googleusercontent.com')
+      }],
+      onError: (err) => {
+        console.log(err)
+      }
+    } as SocialAuthServiceConfig
+  }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
